8d34ccf4ed58fe679ed341848726311f6064759d
Sashko Stubailo <sashko@mit.edu>
2014-04-26 11:43:26 -0400

    Added visit counter

--- extension/redirect.js
+++ extension/redirect.js
@@ -1,3 +1,10 @@
+if (! localStorage.visitCount) {
+  localStorage.visitCount = 0;
+}
+
+localStorage.visitCount = parseInt(localStorage.visitCount, 10) + 1;
+console.log(window.location, localStorage.visitCount);
+
 if (window.location.host.indexOf("facebook") >= 0) {
     window.location = "http://meteor.com";
 }
