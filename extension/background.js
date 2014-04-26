9300c0ebc628e33196d2c363976be3a78f6b9b01
Angela Zhang <zhangela@mit.edu>
2014-04-26 15:25:39 -0400

    restructured slightly

--- extension/background.js
+++ extension/background.js
@@ -1,21 +1,27 @@
 var stayfocused = {};
 
-stayfocused.shouldWorkOut = function() {
-
+stayfocused.isOnBlockedSite = function() {
     chrome.storage.sync.get("BLOCKED_URLS", function (urls) {
         var blockedUrls = urls["BLOCKED_URLS"] || [];
         console.log(blockedUrls);
         var current_url = location.href;
 
-        console.log(location.href);
-
         blockedUrls.forEach(function(blockedUrl) {
-        if (current_url.indexOf(blockedUrl) > -1) {
-            alert("you should work out!");
-        }
+            if (current_url.indexOf(blockedUrl) > -1) {
+                return true;
+            }
+        });
     });
+};
 
-    });
+stayfocused.hasTimerRanOut = function() {
+    return true;
+};
+
+stayfocused.shouldWorkOut = function() {
+    if (stayfocused.isOnBlockedSite() && stayfocused.hasTimerRanOut()) {
+        alert("you should work out!");
+    }
 };
 
 stayfocused.shouldWorkOut();
