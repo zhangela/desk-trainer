2bbec19686ec6958322680248a2889721cb7e5e3
Sashko Stubailo <sashko@mit.edu>
2014-04-26 18:41:26 -0400

    Added placeholder workout complete

--- website/website.js
+++ website/website.js
@@ -1,10 +1,18 @@
+Workouts = new Meteor.Collection("workouts");
+
 if (Meteor.isClient) {
   Session.setDefault("timer", 10);
+
+  var workoutCompleted = function () {
+    // do what????
+  };
 
   var tick = function () {
     if (Session.get("playing") && Session.get("timer") > 0) {
       Session.set("timer", Session.get("timer") - 1);
     }
+
+    workoutCompleted();
   };
 
   setInterval(tick, 1000);
@@ -24,6 +32,15 @@ if (Meteor.isClient) {
     },
     "click .pause": function () {
       Session.set("playing", false);
+    }
+  });
+
+  Template.addWorkout.events({
+    "submit form": function (event) {
+      event.preventDefault();
+      var formData = FormUtils.serializeForm(event.target);
+
+      Workouts.insert(formData);
     }
   });
 }
